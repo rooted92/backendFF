3d80e1aeeb460f15c7a3ce386aaca83edc363de5
DrewNilsson <nilsson1322@gmail.com>
2023-03-31 10:28:03 -0700

    Yard Controller/Service Complete

--- Controllers/YardsController.cs
+++ Controllers/YardsController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backendFF.Models;
+using backendFF.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backendFF.Controllers
@@ -10,6 +12,45 @@ namespace backendFF.Controllers
     [Route("[controller]")]
     public class YardsController : ControllerBase
     {
-        
+        private readonly YardsService _data;
+        public YardsController(YardsService dataFromService)
+        {
+            _data = dataFromService;
+        }
+
+        [HttpPost]
+        [Route("AddYard")]
+        public bool AddYard(YardModel newYard)
+        {
+            return _data.AddYard(newYard);
+        }
+
+        [HttpGet]
+        [Route("GetAllYards")]
+        public IEnumerable<YardModel> GetAllYards()
+        {
+            return _data.GetAllYards();
+        }
+
+        [HttpGet]
+        [Route("GetAllYardsByOrganizationID/{organizationID}")]
+        public IEnumerable<YardModel> GetAllYardsByOrganizationID(int organizationID)
+        {
+            return _data.GetAllYardsByOrganizationID(organizationID);
+        }
+
+        [HttpPost]
+        [Route("UpdateYard")]
+        public bool UpdateYard(YardModel yardToUpdate)
+        {
+            return _data.UpdateYard(yardToUpdate);
+        }
+
+        [HttpPost]
+        [Route("DeleteYard")]
+        public bool DeleteYard(YardModel yardToDelete)
+        {
+            return _data.DeleteYard(yardToDelete);
+        }
     }
 }
--- Services/YardsService.cs
+++ Services/YardsService.cs
@@ -2,11 +2,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backendFF.Models;
+using backendFF.Services.Context;
 
 namespace backendFF.Services
 {
     public class YardsService
     {
-        
+        private readonly DataContext _context;
+        public YardsService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool AddYard(YardModel newYard)
+        {
+            _context.Add(newYard);
+
+            return _context.SaveChanges() != 0;
+        }
+
+        public IEnumerable<YardModel> GetAllYards()
+        {
+            return _context.YardInfo;
+        }
+
+        public IEnumerable<YardModel> GetAllYardsByOrganizationID(int organizationID)
+        {
+            return _context.YardInfo.Where(yard => yard.OrganizationID == organizationID);
+        }
+
+        public bool UpdateYard(YardModel yardToUpdate)
+        {
+            _context.Update<YardModel>(yardToUpdate);
+            return _context.SaveChanges() != 0;
+        }
+
+        public bool DeleteYard(YardModel yardToDelete)
+        {
+            yardToDelete.IsDeleted = true;
+            _context.Update<YardModel>(yardToDelete);
+            return _context.SaveChanges() != 0;
+        }
     }
 }
