8199bc433d4ebc1aff6c579651a87dcfd0c7201f
DrewNilsson <nilsson1322@gmail.com>
2023-03-31 14:09:43 -0700

    Trailer Contoller/Service Complete

--- Controllers/TrailerController.cs
+++ Controllers/TrailerController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backendFF.Models;
+using backendFF.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backendFF.Controllers
@@ -10,6 +12,67 @@ namespace backendFF.Controllers
     [Route("[controller]")]
     public class TrailerController : ControllerBase
     {
-        
+        private readonly TrailerService _data;
+        public TrailerController(TrailerService dataFromService)
+        {
+            _data = dataFromService;
+        }
+
+        [HttpPost]
+        [Route("AddTrailer")]
+        public bool AddTrailer(TrailerModel newTrailer)
+        {
+            return _data.AddTrailer(newTrailer);
+        }
+
+        [HttpGet]
+        [Route("GetAllTrailers")]
+        public IEnumerable<TrailerModel> GetAllTrailers()
+        {
+            return _data.GetAllTrailers();
+        }
+
+        [HttpGet]
+        [Route("GetTrailersByPossessionID/{possessionID}")]
+        public IEnumerable<TrailerModel> GetTrailersByPossessionID(int possessionID)
+        {
+            return _data.GetTrailersByPossessionID(possessionID);
+        }
+
+        [HttpGet]
+        [Route("GetTrailersByOrganizationID/{organizationID}")]
+        public IEnumerable<TrailerModel> GetTrailersByOrganizationID(int organizationID)
+        {
+            return _data.GetTrailersByOrganizationID(organizationID);
+        }
+
+        [HttpGet]
+        [Route("GetTrailersInTransitByOrganizationID/{organizationID}")]
+        public IEnumerable<TrailerModel> GetTrailersInTransitByOrganizationID(int organizationID)
+        {
+            return _data.GetTrailersInTransitByOrganizationID(organizationID);
+        }
+
+        [HttpPost]
+        [Route("UpdateTrailer")]
+        public bool UpdateTrailer(TrailerModel trailerToUpdate)
+        {
+            return _data.UpdateTrailer(trailerToUpdate);
+        }
+
+        [HttpPost]
+        [Route("UpdateTrailerToInTransit/{driverID}")]
+        public bool UpdateTrailerToInTransit(TrailerModel trailerToUpdate, int driverID)
+        {
+            return _data.UpdateTrailerToInTransit(trailerToUpdate, driverID);
+        }
+
+        [HttpPost]
+        [Route("DeleteTrailer")]
+        public bool DeleteTrailer(TrailerModel trailerToDelete)
+        {
+            return _data.DeleteTrailer(trailerToDelete);
+        }
+
     }
 }
--- Services/TrailerService.cs
+++ Services/TrailerService.cs
@@ -2,11 +2,64 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backendFF.Models;
+using backendFF.Services.Context;
 
 namespace backendFF.Services
 {
     public class TrailerService
     {
-        
+        private readonly DataContext _context;
+        public TrailerService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool AddTrailer(TrailerModel newTrailer)
+        {
+            _context.Add(newTrailer);
+            return _context.SaveChanges() != 0;
+        }
+
+        public IEnumerable<TrailerModel> GetAllTrailers()
+        {
+            return _context.TrailerInfo;
+        }
+
+        public IEnumerable<TrailerModel> GetTrailersByPossessionID(int possessionID)
+        {
+            return _context.TrailerInfo.Where(trailer => trailer.PossessionID == possessionID);
+        }
+
+        public IEnumerable<TrailerModel> GetTrailersByOrganizationID(int organizationID)
+        {
+            return _context.TrailerInfo.Where(trailer => trailer.OrganizationID == organizationID);
+        }
+
+        public IEnumerable<TrailerModel> GetTrailersInTransitByOrganizationID(int organizationID)
+        {
+            return _context.TrailerInfo.Where(trailer => trailer.OrganizationID == organizationID && trailer.InTransit == true);
+        }
+
+        public bool UpdateTrailer(TrailerModel trailerToUpdate)
+        {
+            _context.Update<TrailerModel>(trailerToUpdate);
+            return _context.SaveChanges() != 0;
+        }
+
+        public bool UpdateTrailerToInTransit(TrailerModel trailerToUpdate, int driverID)
+        {
+            trailerToUpdate.PossessionID = driverID;
+            trailerToUpdate.InTransit = true;
+            return UpdateTrailer(trailerToUpdate);
+        }
+
+
+
+        public bool DeleteTrailer(TrailerModel trailerToDelete)
+        {
+            trailerToDelete.IsDeleted = true;
+            return UpdateTrailer(trailerToDelete);
+        }
     }
 }
